2ceab9edb9edb6cc4aef11a6664cfae5c1aa4c62
Ghislain Deffo <g.avet.gnaritas@gmail.com>
2021-01-18 19:26:35 -0500

    added a default constructor to Page

--- src/main/java/core/utils/Page.java
+++ src/main/java/core/utils/Page.java
@@ -12,13 +12,22 @@ public final class Page<T> {
     private long number, total, size;
     private Collection<T> content;
 
+    private Page(){
+        number = 0;
+        size = 1;
+        total = 0;
+    }
+
     public <R> Page<R> map(Function<T, R> mapper) {
-        Page<R> newPage = new Page<>(number, total, size,
-                content.stream().map(mapper).collect(Collectors.toList()));
+        Page<R> newPage = new Page<>();
+        newPage.number = number;
+        newPage.total = total;
+        newPage.size = size;
+        newPage.content = content.stream().map(mapper).collect(Collectors.toList());
         return newPage;
     }
 
     public boolean isEmpty() {
-        return content.isEmpty();
+      return content.isEmpty();
     }
 }
